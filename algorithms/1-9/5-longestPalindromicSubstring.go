cc3080176c59adf8800c10f904fd56949c4ebc58
redamancyMS <52806847+redamancyMS@users.noreply.github.com>
2020-10-12 09:58:25 +0800

    Update 5-longestPalindromicSubstring.go

--- algorithms/1-9/5-longestPalindromicSubstring.go
+++ algorithms/1-9/5-longestPalindromicSubstring.go
@@ -9,11 +9,12 @@
 输入: "cbbd"
 输出: "bb"
 
-//分别以s[i]为中心向两边进行扩展找回文子串，遍历结束后，找到的最长子串就是S中的最长回文子串
+//分别以s[i]为中心向两边进行扩展找回文子串，奇偶情况都要考虑到，遍历结束后，找到的最长子串就是S中的最长回文子串
 
 func longestPalindrome(s string) string {
     var res string
     for i := 0 ; i < len(s) ; i++ {
+        //奇数情况
         l := i - 1 
         r := i + 1
         for l >= 0 && r < len(s) && s[l] == s[r] {
@@ -23,6 +24,7 @@ func longestPalindrome(s string) string {
         if len(res) < r-l-1 {
             res = s[l+1:r]
         }
+        //偶数情况
         l = i
         r = i+1
         for l >= 0 && r < len(s) && s[l] == s[r] {
